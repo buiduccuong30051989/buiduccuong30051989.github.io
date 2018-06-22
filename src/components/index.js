6dd100435f848d0d053923b086ce311dae48c43f
bui.duc.cuong <bui.duc.cuong@framgia.com>
2018-06-22 13:48:58 +0700

    ffs

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react"
 
 const App = () => {
   return <div>
-    <div className="card" style="width: 18rem;">
+    <div className="card">
       <div className="card-header">
         Featured
       </div>
