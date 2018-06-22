85abd0b8bce7a22b542fee2f4a4a2c29274d64e4
bui.duc.cuong <bui.duc.cuong@framgia.com>
2018-06-22 13:47:48 +0700

    Fingers crossed!

--- src/components/index.js
+++ src/components/index.js
@@ -9,7 +9,6 @@ const App = () => {
       <ul className="list-group list-group-flush">
         <li className="list-group-item">Cras justo odio</li>
         <li className="list-group-item">Dapibus ac facilisis in</li>
-        <li className="list-group-item">Vestibulum at eros</li>
       </ul>
     </div>
   </div>;
